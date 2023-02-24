fd3b7f7414ab48ccf27d4623d8c744d351460939
AdyatamaMH <114073452+AdyatamaMH@users.noreply.github.com>
2023-02-24 16:02:55 +0700

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,13 @@
+package assignment;
+
+abstract class Shape {
+    public String color;
+
+    public Shape(String color) {
+       this.color = "Gay v.2";
+    }
+
+    public int getArea() {
+        return 0;
+    }
+}
